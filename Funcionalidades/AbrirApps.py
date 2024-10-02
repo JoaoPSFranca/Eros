0a538bb428439a0a30b53f9d599995af93fdcb34
Joao Pedro <francasjoaopedro@gmail.com>
2024-10-01 22:47:38 -0300

    Update Open Apps
    Adicionando a funcionalidade para abrir o lol

--- Funcionalidades/AbrirApps.py
+++ Funcionalidades/AbrirApps.py
@@ -8,3 +8,6 @@ def abrir_aplicativo(comando):
     elif 'abrir bloco de notas' in comando.lower():
         os.startfile("notepad.exe")
         fala.falar("Abrindo Bloco de Notas.")
+    elif ('abrir lol' in comando.lower()) or ('abrir league of legends' in comando.lower()):
+        os.startfile("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Riot Games\\League of Legends")
+        fala.falar("Abrindo League of Legends.")
