efa19bc1074a9476a0a0d15ecb6a6d61b77a7f1c
JoaoPSFranca <francasjoaopedro@gmail.com>
2025-02-21 12:36:19 -0300

    Definindo a estrutura

--- Funcionalidades/OuvirComando.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-
-def ouvir_comando():
-    reconhecedor = sr.Recognizer()
-    with sr.Microphone() as fonte:
-        print("Diga algo:")
-        audio = reconhecedor.listen(fonte)
-    return audio
--- Funcionalidades/SinteseFala.py
+++
@@ -1,6 +0,0 @@
-import pyttsx3
-
-def falar(texto):
-    engine = pyttsx3.init()
-    engine.say(texto)
-    engine.runAndWait()
