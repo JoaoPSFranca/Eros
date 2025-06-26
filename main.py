f13f17e1efa42e23711a4628e622c71c6f8010f2
JoaoPSFranca <francasjoaopedro@gmail.com>
2025-06-25 23:51:59 -0300

    Clean Project | New Start v3 | Config .gitignore

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.core import Assistant
-
-def main():
-    print('\033[36m' + "Iniciando Assistente Virtual..." + '\033[0;0m')
-    assistant = Assistant()
-    assistant.start()
-
-if __name__ == "__main__":
-    main()
